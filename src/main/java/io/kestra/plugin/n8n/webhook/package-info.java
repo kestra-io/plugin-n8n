e7f7ea143dee037baff18bd641340aac34164f24
stevenmeek <stevenmeek0@outlook.com>
2025-11-10 22:07:08 +0000

    chore: add sub-group for the webhook package.

--- src/main/java/io/kestra/plugin/n8n/webhook/package-info.java
+++ src/main/java/io/kestra/plugin/n8n/webhook/package-info.java
@@ -1,8 +1,8 @@
 @PluginSubGroup(
     title = "N8N",
-    description = "Tasks for integrating with n8n automation platform.",
+    description = "Trigger n8n workflows through HTTP Webhook endpoints.",
     categories = PluginSubGroup.PluginCategory.TOOL
 )
-package io.kestra.plugin.n8n;
+package io.kestra.plugin.n8n.webhook;
 
 import io.kestra.core.models.annotations.PluginSubGroup;
